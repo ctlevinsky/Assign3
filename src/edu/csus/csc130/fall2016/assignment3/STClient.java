dbdf14b9098cfd2745b70cbd9308eaf98499e88b
Charles Levinsky <ctlevinsky@gmail.com>
2016-11-07 06:56:40 -0800

    Created Repository

---
+++ src/edu/csus/csc130/fall2016/assignment3/STClient.java
@@ -0,0 +1,13 @@
+package edu.csus.csc130.fall2016.assignment3;
+
+public class STClient {
+	/**
+	 * @param grades arrays of letter grades, assume not null or empty
+	 * @return the average of grades, ignore invalid letter in calculation
+	 */
+	//Q9
+	public  static double getAverage(String[] grades) {
+		throw new UnsupportedOperationException();
+	}
+
+}
